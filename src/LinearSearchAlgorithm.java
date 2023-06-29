478f3c5fe9a0f8dcea5f3d6c399582101fa37f00
Pamelaofurum <132493479+Pamelaofurum@users.noreply.github.com>
2023-06-29 07:48:06 +0200

    commit changed files

--- src/LinearSearchAlgorithm.java
+++ src/LinearSearchAlgorithm.java
@@ -1,59 +1,66 @@
 import java.util.Arrays;
 import java.util.Scanner;
+/**
+ * 
+ * @author Pamela Ofurum
+ *
+ */
 
+//This class implements Multiple Search Algorithms
 public class LinearSearchAlgorithm {
-	// Linear search implementation
+	
+	// This method implements the Linear search
 	public static int linearSearch(int[] arr, int key) {
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == key) {
-				return i; // Found the key at index i
+				return i; // If Key is Found at index i
 			}
 		}
-		return -1; // Key not found
+		return -1; // If Key not found
 	}
 
-	// Interpolation search implementation
+	// This method implements Interpolation search
 	public static int interpolationSearch(int[] arr, int key) {
-		int low = 0;
-		int high = arr.length - 1;
+		int lowSearch = 0;
+		int highSearch = arr.length - 1;
 
-		while (low <= high && key >= arr[low] && key <= arr[high]) {
-			if (low == high) {
-				if (arr[low] == key) {
-					return low; // Found the key at index low
+		while (lowSearch <= highSearch && key >= arr[lowSearch] && key <= arr[highSearch]) {
+			if (lowSearch == highSearch) {
+				if (arr[lowSearch] == key) {
+					return lowSearch; // If key is Found at index lowSearch
 				}
-				return -1; // Key not found
+				return -1; // If Key not found
 			}
 
-			// Calculate the position using interpolation formula
-			int position = low + ((key - arr[low]) * (high - low)) / (arr[high] - arr[low]);
+			// Calculates the position using interpolation formula
+			int position = lowSearch + ((key - arr[lowSearch]) * (highSearch - lowSearch)) / (arr[highSearch] - arr[lowSearch]);
 
 			if (arr[position] == key) {
-				return position; // Found the key at position
+				return position; // If the Key is Found at position
 			} else if (arr[position] < key) {
-				low = position + 1; // Key may be in the upper part
+				lowSearch = position + 1; // If Key is in the upper part
 			} else {
-				high = position - 1; // Key may be in the lower part
+				highSearch = position - 1; // If Key is in the lower part
 			}
 		}
 
-		return -1; // Key not found
+		return -1; // If Key not found
 	}
 
-	// Linear search implementation with transposition
+	// This method implements Linear search with transposition for improvement
 	public static int linearSearchWithTransposition(int[] arr, int key) {
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == key) {
 				if (i > 0) {
-					// Move the found element one position closer to the beginning
+					// Moves the search element one position closer to the beginning of the array
 					int temp = arr[i];
 					arr[i] = arr[i - 1];
 					arr[i - 1] = temp;
 				}
-				return i; // Found the key at index i
+				return i; // If the key is Found at index i
 			}
 		}
-		return -1; // Key not found
+		return -1; // If Key is not found
 	}
 
 	public static void main(String[] args) {
@@ -71,7 +78,7 @@ public class LinearSearchAlgorithm {
 		System.out.print("Enter the search key: ");
 		int key = scanner.nextInt();
 
-		// Perform linear search
+		// Performs a linear search and checks the running time
 		long linearStartTime = System.nanoTime();
 		int linearResult = linearSearch(arr, key);
 		long linearEndTime = System.nanoTime();
@@ -83,10 +90,10 @@ public class LinearSearchAlgorithm {
 			System.out.println("Search key NOT FOUND");
 		}
 
-		// Sort the array
+		// Sorts the array in ascending order
 		Arrays.sort(arr);
 
-		// Perform interpolation search
+		// Performs interpolation search and checks the running time
 		long interpolationStartTime = System.nanoTime();
 		int interpolationResult = interpolationSearch(arr, key);
 		long interpolationEndTime = System.nanoTime();
@@ -98,7 +105,7 @@ public class LinearSearchAlgorithm {
 			System.out.println("Search key NOT FOUND");
 		}
 
-		// Perform linear search with transposition
+		// Performs linear search with transposition for improvement and takes the start time and end time
         long improvedlinearStartTime = System.nanoTime();
         int improvedlinearResult = linearSearchWithTransposition(arr, key);
         long improvedlinearEndTime = System.nanoTime();
@@ -110,7 +117,7 @@ public class LinearSearchAlgorithm {
             System.out.println("Search key NOT FOUND");
         }
         
-		// Calculate running times
+		// Calculates running times
 		long linearRunningTime = linearEndTime - linearStartTime;
 		long interpolationRunningTime = interpolationEndTime - interpolationStartTime;
 		long improvedlinearRunningTime = improvedlinearEndTime - improvedlinearStartTime;
